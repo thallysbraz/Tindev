3b28845f9121c9cd25b8d60a0d9eeb1731af6a2f
thallysbraz <thallysbraz3@gmail.com>
2019-12-03 15:14:43 -0300

    adicionando e finalizando componente Main

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,31 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./Main.css";
 import logo from "../assets/logo.svg";
 import like from "../assets/like.svg";
 import dislike from "../assets/dislike.svg";
 
+import api from "../services/api";
+
 export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get("/devs", {
+        headers: {
+          user: match.params.id
+        }
+      });
+      console.log("response.data: ", response.data);
+      setUsers(response.data);
+    }
+    loadUsers();
+  }, [match.params.id]);
+
   return (
     <div>
       <div className="main-container">
         <img src={logo} alt="Tindev" />
         <ul>
-          <li>
-            <img src={logo} alt="Tindev" />
-            <footer>
-              <strong>Thallys Braz</strong>
-              <p>Estudante</p>
-            </footer>
-            <div className="buttons">
-              <button type="button" alt="Dislike">
-                <img src={dislike} />
-              </button>
-              <button type="button" alt="Like">
-                <img src={like} />
-              </button>
-            </div>
-          </li>
+          {users.map(user => (
+            <li key={user._id}>
+              <img src={user.avatar} alt={user.name} />
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
+              <div className="buttons">
+                <button type="button" alt="Dislike">
+                  <img src={dislike} />
+                </button>
+                <button type="button" alt="Like">
+                  <img src={like} />
+                </button>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
